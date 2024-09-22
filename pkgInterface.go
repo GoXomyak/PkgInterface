8d9b17169a2afbd840c7bcaf9769a530c67e6d8d
GoXomyak <drykilllogic232a@gmail.com>
2024-09-23 00:36:51 +0300

    new

---
+++ pkgInterface.go
@@ -0,0 +1,24 @@
+package mypkg
+
+import "fmt"
+
+type MyInterface interface {
+	MethodWithoutParameters()
+	MethodWithParameters(float64)
+	MethodWithReturnValue() string
+}
+type MyType int
+
+func (m MyType) MethodWithoutParameters() {
+	fmt.Println("MethodWithoutParameters called")
+}
+func (m MyType) MethodWithParameters(f float64) {
+	fmt.Println("MethodWithParameters called", f)
+}
+func (m MyType) MethodWithReturnValue() string {
+	return "MethodWithReturnValue called"
+
+}
+func (m MyType) MethodNotInInterface() {
+	fmt.Println("MethodNotInInterface called")
+}
